385300f88aa97abd62a4205dc2d16eec54190078
wanggege919 <1914161290@qq.com>
2018-11-28 17:59:23 +0800

    1.封装Ajax最终版 2.设置requestHeader 3.如何传不同类型的参数

--- main.js
+++ main.js
@@ -4,9 +4,27 @@ window.jQuery = function (nodeOrSelector) {
     nodes.html = function () { }
     return nodes
 }
-window.jQuery.ajax = function (url, method, body, successFn, failFn) {
+window.jQuery.ajax = function (options) {//函数名一般叫 options
+    let url 
+    if(arguments.lkength === 1){
+        url = arguments[0]
+    }else if(arguments.length === 2){
+        url = arguments[0]
+        options = arguments[1]
+    }//如何在 options 里面传两种不同类型的参数
+    let method = options.method
+    let body = options.body
+    let successFn = options.successFn
+    let failFn = options.failFn
+    let headers = options.headers
+    
+
     let request = new XMLHttpRequest()
     request.open(method, url)//初始化 request 对象 请求方法可以随意写
+    for(let key in headers){
+        let value = headers[key]
+        request.setRequestHeader(key,value)//每次取到一组key value ,就设置一下
+    }//for in 遍历key value
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
             if (request.status >= 200 && request.status < 300) {
@@ -21,16 +39,25 @@ window.jQuery.ajax = function (url, method, body, successFn, failFn) {
 }
 
 window.$ = window.jQuery
+function f1(){}
+function f2(){}
 
 myButton.addEventListener('click', function () {
-    window.jQuery.ajax(
-        '/xxx',
-        'POST',
-        'a=1&b=2', //null/undefined
-        (responseText)=>{console.log(1)},
-        (request)=>{console.log(2)})
-        //这种形式是非常傻的,比如是get方法,往往没有第四部分,但是它的位置已经限制死啦,
-        //就必须用 null/undefined 占位置,失败函数不想要,还得用 null/undefined 占位置
-        //非常不灵活
-        // 最好传一个有结构的参数,  所以是数组,只有数组才有结构嘛
+    let obj = { //给参数命名,在 js 中若是想给参数 选项命名, 一般用这种方法
+        url: '/xxx',
+        method: 'get',
+        headers: {
+            'content-type': 'x-www-form-urlencoded',
+            'wanggege': '18'
+        },//设置 header
+        successFn: (x)=>{//回调函数 callback
+            f1.call(undefined,x)//成功时执行两个函数,依次调用即可
+            f1.call(undefined,x)
+        },
+        failFn: (x)=>{
+            console.log(x)
+        }
+    }
+    window.jQuery.ajax(obj)
+        
 })
